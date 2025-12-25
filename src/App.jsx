6287b39d02005a5d2b9087ab0ca1b4bbd3f7c0a0
Juvert Jay Fetalino <jayfetalino@gmail.com>
2025-12-25 17:31:28 -0500

    feat:wire HabitForm to persistent habit state in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+// Root component that owns habit state and coordinate child component
+
+import { useLocalStorageState } from "./hooks/useLocalStorageState";
+import { getTodayKey } from "./utils/dates";
+import HabitForm from "./components/HabitForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [habits, setHabits] = useLocalStorageState("habits", []);
+
+  function addHabits(name) {
+    const newHabit = {
+      id: crypto.randomUUID(),
+      name: name,
+      createdAt: getTodayKey(),
+      completions: [],
+    };
+
+    setHabits([...habits, newHabits]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Habit Tracker</h1>
+      <HabitForm onAddhabit={addHabits} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/utils/dates.js
+++ src/utils/dates.js
@@ -11,7 +11,7 @@ export function getTodayKey() {
 
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, "0");
-    const today = String(today.getDate()).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
 
     return `${year}-${month}-${day}`;
 }
