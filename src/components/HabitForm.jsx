70ebaba7630d1e571a94c40b709e1e5f5430c2bf
Juvert Jay Fetalino <jayfetalino@gmail.com>
2025-12-26 21:50:11 -0500

    feat: disabel add button when habit name is empty

--- src/components/HabitForm.jsx
+++ src/components/HabitForm.jsx
@@ -1,33 +1,69 @@
 // HabitForm: add a habit with validations
 // Responsible for collecting a new habit name from the user.
 
+// import { useState } from "react";
+
+// function HabitForm({ onAddhabit }) {
+//     const [name, setName] = useState("");
+
+//     function handleSubmit(event) {
+//         event.preventDefault();
+
+//         if (name.trim() === "") {
+//             return;
+//         }
+
+//         onAddhabit(name);
+//         setName("");
+//     }
+
+//     return ( 
+//         <form onSubmit={handleSubmit}>
+//             <input 
+//                 type="text"
+//                 placeholder="Add a new habit"
+//                 value={name}
+//                 onChange={(event) => setName(event.target.value)}
+//             />
+//             <button type="submit">Add</button>
+//         </form>
+//     );
+// }
+
+// export default HabitForm;
+
 import { useState } from "react";
 
-function HabitForm({ onAddhabit }) {
+function HabitForm({ onAddHabit }) {
     const [name, setName] = useState("");
+
+    const isValid = name.trim() !== "";
 
     function handleSubmit(event) {
         event.preventDefault();
 
-        if (name.trim() === "") {
+        if (!isValid) {
             return;
         }
 
-        onAddhabit(name);
+        onAddHabit(name.trim());
         setName("");
     }
 
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text"
-                placeholder="Add a new habit"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-            />
-            <button type="submit">Add</button>
-        </form>
-    );
+return (
+    <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        placeholder="Add a new habit"
+        value={name}
+        onChange={(event) => setName(event.target.value)} 
+        />
+        <button type="submit" disabled={isValid}>
+            Add
+        </button>
+    </form>
+);
+
 }
 
 export default HabitForm;
